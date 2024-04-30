3eb6a388975a4dbd781fce062186ad31ddd64d02
David Ward <davidmward@mail.com>
2024-04-30 16:15:08 -0700

    print file arguments

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ fn main() {
     // Get arguments and convert to collection (vector of strings)
     let args: Vec<String> = env::args().collect();
 
+    let search_string = &args[1];
+    let file_path = &args[2];
+
+    println!("Search term: {}", search_string);
+    println!("File: {}", file_path);
+
     // Debug macro
     dbg!(args);
 }

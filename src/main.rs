de2308f0c777b85dde47c70bf6741ceebac2f255
David Ward <davidmward@mail.com>
2024-04-30 15:57:07 -0700

    get command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// Bring module into scope for getting command line arguments
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+
+    // Get arguments and convert to collection (vector of strings)
+    let args: Vec<String> = env::args().collect();
+
+    // Debug macro
+    dbg!(args);
 }

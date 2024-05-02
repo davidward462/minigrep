d6e74fea1f8f8511cf445ed56fe2915880d1bc3f
David Ward <davidmward@mail.com>
2024-05-01 23:55:46 -0700

    use clone on strings in config function

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ struct Config {
 
 fn parse_config(args:&[String]) -> Config
 {
-    let query = &args[1];
-    let file_path = &args[2];
+    let query = args[1].clone();
+    let file_path = args[2].clone();
 
     // return the config struct
     Config {query, file_path}
@@ -23,13 +23,13 @@ fn main() {
     // Get arguments and convert to collection (vector of strings)
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Search term: {}", query);
-    println!("File: {}", file_path);
+    println!("Search term: {}", config.query);
+    println!("File: {}", config.file_path);
 
     // Read file
-    let text = fs::read_to_string(file_path).expect("Should have been able to read the file.");
+    let text = fs::read_to_string(config.file_path).expect("Should have been able to read the file.");
 
     // Print text from file
     println!("Text:\n{}", text);

109411afd7b2e259daab0a44f4a60e15514704a9
David Ward <davidmward@mail.com>
2024-05-02 00:33:09 -0700

    improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-// Bring module into scope for getting command line arguments
-use std::env;
-// For reading file
-use std::fs;
+use std::env; // command line arguments
+use std::fs; // read file
+use std::process; // exit process
 
 // Hold program configuration data
 struct Config {
@@ -10,13 +9,20 @@ struct Config {
 }
 
 impl Config {
-    fn new(args:&[String]) -> Config
+    // Return a result with a Config instance on success
+    // The error value is a string literal with the 'static lifetime
+    fn build(args:&[String]) -> Result<Config, &'static str>
     {
+        // check args length
+        if args.len() < 3 {
+            return Err("Usage: cargo run -- <query> <filepath>");
+        }
+
+        // save arguments
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        // return the config struct
-        Config {query, file_path}
+        Ok(Config {query, file_path})
     }
 }
 
@@ -25,8 +31,12 @@ fn main() {
     // Get arguments and convert to collection (vector of strings)
     let args: Vec<String> = env::args().collect();
 
-    // Call new() function implemented on the Config struct
-    let config = Config::new(&args);
+    // Call build() function implemented on the Config struct
+    // The unwrap_or_else calls anonymous function on error
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("{err}");
+        process::exit(1);
+    });
 
     println!("Search term: {}", config.query);
     println!("File: {}", config.file_path);

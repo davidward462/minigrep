173d02b8b87fd75c36f71212b89800fac314f5fe
David Ward <davidmward@mail.com>
2024-05-06 19:16:42 -0700

    update case sensitive  test

--- src/lib.rs
+++ src/lib.rs
@@ -74,15 +74,17 @@ mod tests
     use super::*;
 
     #[test]
-    fn case_sensitive()
-    {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
+
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
     #[test]
     fn case_insensitive() {
         let query = "rUsT";
